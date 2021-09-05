b046c171c9821ad6c211b0e6afa47659683045c8
covers1624 <laughlan.cov@internode.on.net>
2021-09-05 14:45:03 +0930

    Add some useful Deprecation sibling Annotations.

---
+++ src/main/java/net/covers1624/quack/annotation/ReplaceWith.java
@@ -0,0 +1,26 @@
+package net.covers1624.quack.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Intended for use with {@link Deprecated} annotations, to denote
+ * the intended replacement of a method, field, or class.
+ * <p>
+ * This annotation does not currently enforce a contract for the format of
+ * this annotation's value. However, Javadoc rules should apply:
+ * <pre>
+ *     java.lang.String                     // Class
+ *     java.lang.String#toString            // Method without param bound, or field.
+ *     java.lang.String#substring(int, int) // Method with class and param bound.
+ *     #substring(int, int)                 // Method with only a param bound. Assumes the current class.
+ *     #substring                           // Method without a param bound. Assumes the current class.
+ * </pre>
+ * <p>
+ * Created by covers1624 on 5/9/21.
+ */
+@Retention (RetentionPolicy.CLASS)
+public @interface ReplaceWith {
+
+    String value();
+}
---
+++ src/main/java/net/covers1624/quack/annotation/ReplaceWithExpr.java
@@ -0,0 +1,18 @@
+package net.covers1624.quack.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Intended for use with {@link Deprecated} annotations, to denote
+ * the intended replacement of a method, field, or class with the given expression pattern.
+ * <p>
+ * The pattern is in no known format, and intended for a developer to read and understand manually.
+ * <p>
+ * Created by covers1624 on 5/9/21.
+ */
+@Retention (RetentionPolicy.CLASS)
+public @interface ReplaceWithExpr {
+
+    String value();
+}
