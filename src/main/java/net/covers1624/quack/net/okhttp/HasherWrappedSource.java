6cca04af64738fb3a5104c708c3e42f6fed525cc
covers1624 <laughlan.cov@internode.on.net>
2021-11-21 23:01:55 +1030

    Move HasherWrapperSource out of MultiHasherInterceptor

---
+++ src/main/java/net/covers1624/quack/net/okhttp/HasherWrappedSource.java
@@ -0,0 +1,41 @@
+/*
+ * This file is part of Quack and is Licensed under the MIT License.
+ *
+ * Copyright (c) 2018-2021 covers1624 <https://github.com/covers1624>
+ */
+package net.covers1624.quack.net.okhttp;
+
+import net.covers1624.quack.annotation.Requires;
+import net.covers1624.quack.util.MultiHasher;
+import okio.Buffer;
+import okio.ForwardingSource;
+import okio.Source;
+
+import java.io.IOException;
+
+/**
+ * A {@link Source} wrapper, which forwards all bytes passed through into
+ * a {@link MultiHasher}.
+ * <p>
+ * Created by covers1624 on 21/11/21.
+ */
+@Requires ("com.google.guava:guava")
+@Requires ("com.squareup.okhttp3:okhttp")
+public class HasherWrappedSource extends ForwardingSource {
+
+    private final MultiHasher hasher;
+
+    public HasherWrappedSource(Source delegate, MultiHasher hasher) {
+        super(delegate);
+        this.hasher = hasher;
+    }
+
+    @Override
+    public long read(Buffer sink, long byteCount) throws IOException {
+        long bytesRead = super.read(sink, byteCount);
+        if (bytesRead != 1) {
+            hasher.update(sink.peek().readByteArray());
+        }
+        return bytesRead;
+    }
+}
--- src/main/java/net/covers1624/quack/net/okhttp/MultiHasherInterceptor.java
+++ src/main/java/net/covers1624/quack/net/okhttp/MultiHasherInterceptor.java
@@ -10,13 +10,10 @@ import net.covers1624.quack.util.MultiHasher;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
-import okio.Buffer;
-import okio.ForwardingSource;
-import okio.Source;
+import okhttp3.ResponseBody;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.util.Objects;
 
 /**
  * An {@link Interceptor} capable of handling a {@link MultiHasher} {@link Request} tag.
@@ -34,31 +31,14 @@ public class MultiHasherInterceptor implements Interceptor {
         MultiHasher hasher = request.tag(MultiHasher.class);
 
         Response response = chain.proceed(request);
-        if (hasher == null) {
-            return response;
-        }
+        if (hasher == null) return response;
+
+        ResponseBody body = response.body();
+        if (body == null) return response;
 
         return response.newBuilder()
-                .body(SniffingResponseBody.ofFunction(Objects.requireNonNull(response.body()), source -> new HasherWrappedSource(source, hasher)))
+                .body(SniffingResponseBody.ofFunction(body, source -> new HasherWrappedSource(source, hasher)))
                 .build();
     }
 
-    private static class HasherWrappedSource extends ForwardingSource {
-
-        private final MultiHasher hasher;
-
-        public HasherWrappedSource(Source delegate, MultiHasher hasher) {
-            super(delegate);
-            this.hasher = hasher;
-        }
-
-        @Override
-        public long read(Buffer sink, long byteCount) throws IOException {
-            long bytesRead = super.read(sink, byteCount);
-            if (bytesRead != 1) {
-                hasher.update(sink.peek().readByteArray());
-            }
-            return bytesRead;
-        }
-    }
 }
