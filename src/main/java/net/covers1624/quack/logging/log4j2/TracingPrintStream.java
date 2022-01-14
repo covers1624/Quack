0fbaab44b2b39b37bf40c696a984d3d824e540a9
covers1624 <laughlan.cov@internode.on.net>
2022-01-14 21:27:24 +1030

    Remove yearly churn from license headers.

--- src/main/java/net/covers1624/quack/logging/log4j2/TracingPrintStream.java
+++ src/main/java/net/covers1624/quack/logging/log4j2/TracingPrintStream.java
@@ -1,7 +1,5 @@
 /*
  * This file is part of Quack and is Licensed under the MIT License.
- *
- * Copyright (c) 2018-2021 covers1624 <https://github.com/covers1624>
  */
 package net.covers1624.quack.logging.log4j2;
 
--- src/main/java/net/covers1624/quack/platform/package-info.java
+++ src/main/java/net/covers1624/quack/platform/package-info.java
@@ -1,7 +1,5 @@
 /*
  * This file is part of Quack and is Licensed under the MIT License.
- *
- * Copyright (c) 2018-2021 covers1624 <https://github.com/covers1624>
  */
 /**
  * Created by covers1624 on 10/30/21.
