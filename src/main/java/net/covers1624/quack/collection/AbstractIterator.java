f69837ca033be22365f6d99d412f64b795855819
covers1624 <laughlan.cov@internode.on.net>
2023-02-06 13:26:49 +1030

    FastStream! (#2)
    * Completely new implementation of StreamableIterable with fast paths.
    
    
    Co-authored-by: Chicken-Bones <mehvids@gmail.com>
    
    * Fix license headers.
    
    * Fix knownLength docs.
    
    * Fix Function generics on FastStream.toMap methods.
    
    * Add TypeCheck inference hack to collecting calls.
    
    ---------
    
    Co-authored-by: Chicken-Bones <mehvids@gmail.com>

---
+++ src/main/java/net/covers1624/quack/collection/AbstractIterator.java
@@ -0,0 +1,51 @@
+/*
+ * This file is part of Quack and is Licensed under the MIT License.
+ */
+package net.covers1624.quack.collection;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Created by covers1624 on 19/1/23.
+ */
+public abstract class AbstractIterator<T> implements Iterator<T> {
+
+    private static final int CONSUMED = 0;
+    private static final int HAS_NEXT = 1;
+    private static final int END_OF_DATA = 2;
+
+    private int state;
+    @Nullable
+    private T next;
+
+    @Nullable
+    protected abstract T computeNext();
+
+    @Override
+    public final boolean hasNext() {
+        if (state == END_OF_DATA) return false;
+        if (state == CONSUMED) {
+            state = HAS_NEXT;
+            next = computeNext();
+        }
+        return state == HAS_NEXT;
+    }
+
+    @Override
+    public final T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        state = CONSUMED;
+        return next;
+    }
+
+    @Nullable
+    protected final T endOfData() {
+        state = END_OF_DATA;
+        return null;
+    }
+}
---
+++ src/main/java/net/covers1624/quack/collection/ForEachAbort.java
@@ -0,0 +1,17 @@
+/*
+ * This file is part of Quack and is Licensed under the MIT License.
+ */
+package net.covers1624.quack.collection;
+
+/**
+ * Created by covers1624 on 30/1/23.
+ */
+class ForEachAbort extends RuntimeException {
+
+    public static final ForEachAbort INSTANCE = new ForEachAbort();
+
+    @Override
+    public Throwable fillInStackTrace() {
+        return this;
+    }
+}
