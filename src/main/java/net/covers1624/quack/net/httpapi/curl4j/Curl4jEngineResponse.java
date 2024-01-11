3170dd1819bc47b2c9cecb40e59ff345c680868c
covers1624 <covers1624@gmail.com>
2024-01-11 18:17:40 +1030

    Incremental curl downloads with curl_multi.

--- src/main/java/net/covers1624/quack/net/httpapi/curl4j/Curl4jEngineResponse.java
+++ src/main/java/net/covers1624/quack/net/httpapi/curl4j/Curl4jEngineResponse.java
@@ -15,46 +15,8 @@ import java.io.IOException;
  * Created by covers1624 on 1/11/23.
  */
 @Requires ("net.covers1624:curl4j")
-public class Curl4jEngineResponse implements EngineResponse {
-
-    private final Curl4jEngineRequest request;
-    private final int statusCode;
-    private final HeaderList headers;
-    private final WebBody body;
-
-    public Curl4jEngineResponse(Curl4jEngineRequest request, int statusCode, HeaderList headers, WebBody body) {
-        this.request = request;
-        this.statusCode = statusCode;
-        this.headers = headers;
-        this.body = body;
-    }
+public abstract class Curl4jEngineResponse implements EngineResponse {
 
     @Override
-    public Curl4jEngineRequest request() {
-        return request;
-    }
-
-    @Override
-    public int statusCode() {
-        return statusCode;
-    }
-
-    @Override
-    public String message() {
-        return "";
-    }
-
-    @Override
-    public HeaderList headers() {
-        return headers;
-    }
-
-    @Override
-    public @Nullable WebBody body() {
-        return body;
-    }
-
-    @Override
-    public void close() throws IOException {
-    }
+    public abstract Curl4jEngineRequest request();
 }
